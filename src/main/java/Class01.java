2180073304a749a6e27ab599d56830d866f5254f
turko <baltatemin@gmail.com>
2023-05-13 19:36:02 +0300

    v-1.1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,33 @@
+public class Class01 {
+    public static void main(String[] args) {
+
+        int x = 1;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println("Version 1 için islemler tamam!");
+        //terminale yazdıklarım cls veya clean enter la silinir
+        //çıkmak için q ya bas
+        //git init yazdığımda repostory olusturur
+        //henüz version haline gelmediği için kaydolmadığı için kızarır
+        //MyProject01 (sağ tık)>open in> explorer> MyProject>git çıkar
+        //version haline getirmek için(working space çalışma alanından staging area ya göndermek için) "git add ."
+        //git status => durum bildirir
+        //git add .  => enter steaging area da version olusturur
+        //git commit -m "Version 1"  => enter
+
+        //Version 2 çalısmaları
+        int z = 4;
+        System.out.println(x+y+z);
+
+        //V3
+        System.out.println("V3 kodları...");
+        //V4
+        System.out.println("V4 çaşlışmaları");
+
+        //MyProject01 ilk kayıt çalışmam
+        //String input = "ilk defa yükleme yapcam";
+        //09 may_deneme 01
+
+
+    }//main method
+}//class
